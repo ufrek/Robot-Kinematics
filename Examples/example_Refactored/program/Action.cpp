e2b2b867e39ed834986942802925eab1b730d5bc
fwhitney <whitney.ferris@gmail.com>
2022-04-05 23:16:49 -0400

    Action.cpp update

--- Examples/example_Refactored/program/Action.cpp
+++ Examples/example_Refactored/program/Action.cpp
@@ -16,6 +16,33 @@
 #include "GaussianMixModel.cpp"
 
 std::vector<MotorModel> motorModels;
+std::vector<double> weights0;
+std::vector<double> weights1;
+std::vector<double> weights2;
+std::vector<double> weights3;
+std::vector<double> weights4;
+std::vector<double> weights5;
+std::vector<double> centers0;
+std::vector<double> centers1;
+std::vector<double> centers2;
+std::vector<double> centers3;
+std::vector<double> centers4;
+std::vector<double> centers5;
+std::vector<double> standard_deviations0;
+std::vector<double> standard_deviations1;
+std::vector<double> standard_deviations2;
+std::vector<double> standard_deviations3;
+std::vector<double> standard_deviations4;
+std::vector<double> standard_deviations5;
+
+std::vector<MotorModel> motorModels;
+MotorModel model0;
+MotorModel model1;
+MotorModel model2;
+MotorModel model3;
+MotorModel model4;
+MotorModel model5;
+
 Action::Action(const char* inpath[9])
 {
 	std::vector<double> weights;
@@ -42,6 +69,7 @@ Action::Action(const char* inpath[9])
 	int weightcount = data[0]; // Gets the motor weight count from the first element in the file.
 	int linecount = weightcount * 6; // Calculates total numbers of line to read.
 	
+	int counter = 0;
 	// Reads the remaining lines of the csv file and saves them into 3 vectors.
 	for (int i = 1; i < linecount; i++) {
 		int j = 0;
@@ -58,12 +86,64 @@ Action::Action(const char* inpath[9])
 				var = var + x;
 			}
 		}
+		counter++;
 		printf("%f %f %f \n", data[0], data[1], data[2]);
 
 		weights.push_back(data[0]);
 		centers.push_back(data[1]);
 		standard_deviations.push_back(data[2]);
+		int vectorcounter = 0;
+		if (counter = weightcount) {
+			vectorcounter = 0;
+			if (counter == 0) {
+				std::copy(weights.begin(), weights.end(), std::back_inserter(weights0));
+				std::copy(centers.begin(), centers.end(), std::back_inserter(centers0));
+				std::copy(standard_deviations.begin(), standard_deviations.end(), std::back_inserter(standard_deviations0));
+			}
+			else if (counter == 1) {
+				std::copy(weights.begin(), weights.end(), std::back_inserter(weights1));
+				std::copy(centers.begin(), centers.end(), std::back_inserter(centers1));
+				std::copy(standard_deviations.begin(), standard_deviations.end(), std::back_inserter(standard_deviations1));
+			}
+			else if (counter == 2) {
+				std::copy(weights.begin(), weights.end(), std::back_inserter(weights2));
+				std::copy(centers.begin(), centers.end(), std::back_inserter(centers2));
+				std::copy(standard_deviations.begin(), standard_deviations.end(), std::back_inserter(standard_deviations2));
+			}
+			else if (counter == 3) {
+				std::copy(weights.begin(), weights.end(), std::back_inserter(weights3));
+				std::copy(centers.begin(), centers.end(), std::back_inserter(centers3));
+				std::copy(standard_deviations.begin(), standard_deviations.end(), std::back_inserter(standard_deviations3));
+			}
+			else if (counter == 4) {
+				std::copy(weights.begin(), weights.end(), std::back_inserter(weights4));
+				std::copy(centers.begin(), centers.end(), std::back_inserter(centers4));
+				std::copy(standard_deviations.begin(), standard_deviations.end(), std::back_inserter(standard_deviations4));
+			}
+			else if (counter == 5) {
+				std::copy(weights.begin(), weights.end(), std::back_inserter(weights5));
+				std::copy(centers.begin(), centers.end(), std::back_inserter(centers5));
+				std::copy(standard_deviations.begin(), standard_deviations.end(), std::back_inserter(standard_deviations5));
+			}
+			weights.clear();
+			centers.clear();
+			standard_deviations.clear();
+			vectorcounter++;
+
+		}
 	}
+	model0 = *new MotorModel(weights0, centers0, standard_deviations0);
+	motorModels.push_back(model0);
+	model1 = *new MotorModel(weights1, centers1, standard_deviations1);
+	motorModels.push_back(model1);
+	model2 = *new MotorModel(weights2, centers2, standard_deviations2);
+	motorModels.push_back(model2);
+	model3 = *new MotorModel(weights3, centers3, standard_deviations3);
+	motorModels.push_back(model3);
+	model4 = *new MotorModel(weights4, centers4, standard_deviations4);
+	motorModels.push_back(model4);
+	model5 = *new MotorModel(weights5, centers5, standard_deviations5);
+	motorModels.push_back(model5);
 	indata.close();
 }
 
@@ -78,7 +158,7 @@ double Action::angleFromGaussian(MotorModel m, float duration)
 
 	for (int i = 0; i < 14; i++)  //
 	{
-		out = out + weights[i] * exp(-(pow((duration - centers[13 - i]), 2)) / (2 * pow(std[13 - i], 2)));    //centers an std are reversed to make the dot product work
+		out = out + weights[i] * exp(-(pow((duration - centers[13 - i]), 2)) / (2 * pow(stdevs[13 - i], 2)));    //centers an std are reversed to make the dot product work
 
 	}
 	out = out + weights[14]; //make sure to include intercept
