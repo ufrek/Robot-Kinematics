14a912d4c5b5396d7b21fb6cc19baad11f323979
ufrek <robert.ks.email@gmail.com>
2022-04-05 21:31:27 -0400

    refactored copy

---
+++ Examples/example_Refactored - Copy/program/DataSet.cpp
@@ -0,0 +1,23 @@
+#include "DataSet.h"
+#include <iostream>
+
+void cradleToHomeAngles(float duration)
+{
+	std::cout << "DataSet needs to be specified";
+}
+
+void DrawAngles(float duration)
+{
+	std::cout << "DataSet needs to be specified";
+}
+
+void homeToCradleAngles(float duration)
+{
+	std::cout << "DataSet needs to be specified";
+}
+
+void CalculateMotorAngles(int actionIndex)
+{
+	std::cout << "DataSet needs to be specified";
+
+}
